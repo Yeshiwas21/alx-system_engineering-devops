c49b68be9b36a8093ce7131c697535c9cf5f580c
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-10-03 19:52:59 +0300

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#the regular expression that will match
+#Repetition Token task 3
+puts ARGV[0].scan(/hbt+n/).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#the regular expression that will match
+# repetitive token task 4
+puts ARGV[0].scan(/hbt*n/).join
