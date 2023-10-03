90e6d30268a6e940356e1e05d64882b31860d7ca
Yeshiwas21 <58876309+Yeshiwas21@users.noreply.github.com>
2023-10-03 19:57:07 +0300

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-#The regular expression that will match
-#repetitive token
-puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-#the regular expression that will match
-# repetitive token task 4
-puts ARGV[0].scan(/hbt*n/).join
