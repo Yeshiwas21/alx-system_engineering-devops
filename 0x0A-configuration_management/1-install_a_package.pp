991220dfc324bcb9c068e95545b4c06ce23fdc46
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-11-24 15:36:45 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,10 @@
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure the 'python3-pip' package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
 # Use pip3 to install Flask with the specified version
 package { 'flask':
   ensure  => '2.1.0',
