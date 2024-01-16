39016abdea82a0b474f5c19255296891c0841858
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-01-16 14:56:46 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number
+This script queries the Reddit API and returns the number
 of subscribers for a given subreddit.
 """
 
 import requests
 
 def number_of_subscribers(subreddit):
-    """Retrieves the number of subscribers for a given subreddit."""
+    """
+    Retrieves the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers or 0 if the subreddit is invalid.
+    """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
