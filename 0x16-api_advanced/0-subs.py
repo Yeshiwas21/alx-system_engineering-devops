c0023ace233b5c4faedd78ebba3d4aba0407cd31
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-01-16 14:55:03 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,15 @@ of subscribers for a given subreddit.
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Retrieves the number of subscribers for a given subreddit."""
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    url = requests.get('http://www.reddit.com/r/{}/about.json'
-                       .format(subreddit),
-                       headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /b/bicky)'}).json()
+
+    url = requests.get(
+        f'http://www.reddit.com/r/{subreddit}/about.json',
+        headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /b/bicky)'}
+    ).json()
+
     subs = url.get("data", {}).get("subscribers", 0)
     return subs
