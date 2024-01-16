a70dc3c8579ef7c35f3e09820650c5667c2981dc
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-01-16 15:21:00 +0300

    all

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+"""Query Reddit API to determine subreddit sub count
 """
-function that queries Reddit API and returns
-number of subscribers (not active users, total subscribers)
-"""
-import json
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ return number of subscribers """
-    url = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit), headers={"User-Agent": "kalkidan"})
-    if url.status_code == 200:
-        return url.json().get("data").get("subscribers")
-    else:
+    """Request number of subscribers of subreddit
+    from Reddit API
+    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return 0
+
+    # load response unit from json
+    data = r.json()['data']
+    # extract list of pages
+    pages = data['children']
+    # extract data from first page
+    page_data = pages[0]['data']
+    # return number of subreddit subs
+    return page_data['subreddit_subscribers']
